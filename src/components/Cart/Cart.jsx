1fc162b09ecb4a31224d241e2c373f09dddd2165
Fattahil-Tanha <Fattahil-Tanha>
2023-04-08 22:46:01 +0600

    routing done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = (props) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        total = total + product.price
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity
         totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity;
     }
     const tax= total*7/100;
     const grandTotal= total + totalShipping + tax;
     return (
         <div className='cart'>
             <h4 className='heading'>Orders Summery</h4>
-                <p className='selected-items'>Selected items : {cart.length}</p>
+                <p className='selected-items'>Selected items : {quantity}</p>
                 <p>total price : ${total}</p>
                 <p>total shipping : ${totalShipping}</p>
                 <p>tax : ${tax.toFixed(2)}</p>
